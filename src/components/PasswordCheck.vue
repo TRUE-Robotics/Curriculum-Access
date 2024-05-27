4a43fa87c068033439efd000a5116c65fdf473eb
Anthony Galgano <anthonygalgano@Anthonys-MacBook-Pro.local>
2024-05-26 23:39:31 -0400

    new password check

---
+++ src/components/PasswordCheck.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-if="authenticated">
+      <router-view></router-view>
+    </div>
+    <div v-else>
+      <h1>Access Denied</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      authenticated: false,
+    };
+  },
+  created() {
+    this.checkPassword();
+  },
+  methods: {
+    checkPassword() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const password = urlParams.get("password");
+      const envPassword = process.env.VUE_APP_ACCESS_PASSWORD;
+      if (password === envPassword) {
+        this.authenticated = true;
+      } else {
+        this.authenticated = false;
+      }
+    },
+  },
+};
+</script>
